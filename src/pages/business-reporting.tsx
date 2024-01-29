4748139c118162ec66159cfd7c93d529b0afdc59
Rachel McMahan <rae.mcmahan@gmail.com>
2024-01-28 17:23:17 -0800

    Added Power BI report to Projects. Added Bootstrap dependency to clean up the business-reportingpage so it would at least be responsive.

---
+++ src/pages/business-reporting.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+const BusinessReportingProjectPage = () => {
+    return (
+        <main class="container">
+            <h1>Business Reporting</h1>
+
+            <iframe title="Report Section"
+                width="900"
+                height="560.25"
+                src="https://app.powerbi.com/view?r=eyJrIjoiMzZkNWQ4ZDctZTQ3NS00ZjA3LWEzMTctZGY1NDRjMDBmMTEyIiwidCI6IjE5NGZhMzM2LWNhYzMtNDhmOS1hN2Y4LTg2OWMyNDRkN2ZhMSIsImMiOjZ9"
+                frameborder="0"
+                allowFullScreen="true"></iframe>
+        </main>
+    )
+}
+
+export default BusinessReportingProjectPage

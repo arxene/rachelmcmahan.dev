a20c20820cff5098bbd4a2fe7575de92a0bae6e9
Rachel McMahan <rae.mcmahan@gmail.com>
2024-03-07 11:06:12 -0800

    Updated page header. Update PowerBI windows size. Minor wording updates.

--- src/pages/business-reporting.tsx
+++ src/pages/business-reporting.tsx
@@ -4,10 +4,10 @@ const BusinessReportingProjectPage = () => {
     return (
         <main class="container">
             <h1>Business Reporting</h1>
-
-            <iframe title="Report Section"
-                width="900"
-                height="560.25"
+            <br />
+            <iframe title="WideWorldImporters-SalesDashboard"
+                width="1024"
+                height="804"
                 src="https://app.powerbi.com/view?r=eyJrIjoiMzZkNWQ4ZDctZTQ3NS00ZjA3LWEzMTctZGY1NDRjMDBmMTEyIiwidCI6IjE5NGZhMzM2LWNhYzMtNDhmOS1hN2Y4LTg2OWMyNDRkN2ZhMSIsImMiOjZ9"
                 frameborder="0"
                 allowFullScreen="true"></iframe>

b69f7178660112923f3721d8fad2f6a16a31a773
Rachel McMahan <rae.mcmahan@gmail.com>
2024-05-24 17:46:17 -0700

    Minor capitalization typo.

--- src/pages/techstudy.js
+++ src/pages/techstudy.js
@@ -26,7 +26,7 @@ export default function TechStudy() {
         This 2.5 hour tech study is a fun, easy way to earn $225 in your free time! You will test a VR (virtual reality)
         headset by making different facial expressions and saying phrases. The location is near Glendale.
       </p>
-      <p>We're looking to have a variety of different people try these Vr headsets:</p>
+      <p>We're looking to have a variety of different people try these VR headsets:</p>
       <table>
         <tr>
           <th>Ethnicity</th>
